9b46cc6381df3b6460cf796dcfd570cfe1c7fd3b
gogwangsin <gwangsin1029@naver.com>
2025-04-16 18:56:30 +0900

    MainActivity를 app package로 이동

--- AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/app/MainActivity.java
+++ AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/app/MainActivity.java
@@ -1,4 +1,4 @@
-package kr.ac.tukorea.ge.and.gwang.stealth;
+package kr.ac.tukorea.ge.and.gwang.stealth.app;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import kr.ac.tukorea.ge.and.gwang.stealth.R;
 
 public class MainActivity extends AppCompatActivity {
 
