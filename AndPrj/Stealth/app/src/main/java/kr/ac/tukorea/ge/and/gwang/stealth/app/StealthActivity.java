4f56b079674dc8147a3f92f0a5ddf60ac5f9b0b4
gogwangsin <gwangsin1029@naver.com>
2025-04-16 19:47:52 +0900

    디버그 빌드일 때 grid 및 FPS 표시 + MainScene class 추가하고 화면에 추가. ( Refactor -> Move -> game패키지 )

--- AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/app/StealthActivity.java
+++ AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/app/StealthActivity.java
@@ -2,12 +2,17 @@ package kr.ac.tukorea.ge.and.gwang.stealth.app;
 
 import android.os.Bundle;
 
+import kr.ac.tukorea.ge.and.gwang.stealth.BuildConfig;
+import kr.ac.tukorea.ge.and.gwang.stealth.game.MainScene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class StealthActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        GameView.drawsDebugStuffs = BuildConfig.DEBUG;
         super.onCreate(savedInstanceState);
+        new MainScene().push();
     }
 }
---
+++ AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/game/MainScene.java
@@ -0,0 +1,9 @@
+package kr.ac.tukorea.ge.and.gwang.stealth.game;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+
+public class MainScene extends Scene {
+    public MainScene(){
+
+    }
+}
