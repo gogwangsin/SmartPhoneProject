f0aaf0444578faae91c45ecf40f3ec2e5f4e9ccb
gogwangsin <gwangsin1029@naver.com>
2025-04-16 19:01:05 +0900

    New Empty View Activity "StealthActivity" + No Layout XML

---
+++ AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/app/StealthActivity.java
@@ -0,0 +1,19 @@
+package kr.ac.tukorea.ge.and.gwang.stealth.app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import kr.ac.tukorea.ge.and.gwang.stealth.R;
+
+public class StealthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
