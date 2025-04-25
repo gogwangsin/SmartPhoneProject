080d214b35bc58dc06f08d9d9a63a94194ca9740
gogwangsin <gwangsin1029@naver.com>
2025-04-25 14:08:17 +0900

    조이스틱을 누를 땐 조이스틱 방향과 power를 이용해해서 이동, 관성( inertiaX )과 속도( velocityY )도 갱신 -> 조이스틱을 놨을 때 x축은 마지막 조이스틱 이동 방향으로 계속 이동, y축은 중력을 적용 받아서 아래로 떨어지도록

--- AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/game/Player.java
+++ AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/game/Player.java
@@ -20,10 +20,9 @@ public class Player extends Sprite {
     private static final float SPEED_X = 700.f;
     private static final float SPEED_Y = 500.f;
     private static final float RADIUS = 100f;
-    private static final float GRAVITY = 300f;
-    private float lastDirX = 0f;
-    private float lastDirY = 1;
-    // 조이스틱 입력이 존재할 때 단위벡터 저장
+    private static final float GRAVITY = 500f;
+    private float inertiaX = 0f; // x 방향 관성
+    private float velocityY = 0f; // y방향 중력 가속도
 
     private float minX, maxX, minY, maxY;
     // 이 경계면을 나가면 안움직인다
@@ -66,14 +65,20 @@ public class Player extends Sprite {
 
         angle = (float)Math.toDegrees(joyStick.angle_radian);
 
+        // 조이스틱 방향을 '단위 벡터'로 변경
+        float dirX = (float)Math.cos(joyStick.angle_radian);
+        float dirY = (float)Math.sin(joyStick.angle_radian);
+
+        // x축 방향의 '현재 관성 속도' 저장
+        // 조이스틱 입력에 따라 "방향 + 세기"를 반영한 x축 이동 속도를 기억
+        inertiaX = SPEED_X * joyStick.power * dirX;  // 관성 갱신
+        velocityY = SPEED_Y * joyStick.power * dirY; // y속도 초기화
+
         float distance_x = SPEED_X * joyStick.power * GameView.frameTime;
         float distance_y = SPEED_Y * joyStick.power * GameView.frameTime;
 
-        lastDirX = (float)Math.cos(joyStick.angle_radian);
-        lastDirY = (float)Math.sin(joyStick.angle_radian);
-
-        float newX = x + distance_x * lastDirX;
-        float newY = y + distance_y * lastDirY;
+        float newX = x + distance_x * dirX;
+        float newY = y + distance_y * dirY;
 
         // 새 위치가 화면 안에 있을 때만 갱신
         if (minX <= newX && newX <= maxX && minY <= newY && newY <= maxY) {
@@ -84,10 +89,11 @@ public class Player extends Sprite {
     }
 
     private void applyGravity() {
-        float distance = GRAVITY * GameView.frameTime;
+        // 중력 가속도 더하기
+        velocityY += GRAVITY * GameView.frameTime;
 
-        float newX = x + distance * lastDirX;
-        float newY = y + distance * lastDirY;
+        float newX = x + inertiaX * GameView.frameTime;
+        float newY = y + velocityY * GameView.frameTime;
 
         // 경계 조건 체크
         if ( minX <= newX && newX <= maxX ) x = newX;
