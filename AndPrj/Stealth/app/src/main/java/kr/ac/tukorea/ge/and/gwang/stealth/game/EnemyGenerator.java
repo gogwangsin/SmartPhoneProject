283bea4d48ed118312486537cce85339be371b8e
gogwangsin <gwangsin1029@naver.com>
2025-04-25 18:02:11 +0900

    EnemyGenerator가 Enemy를 생성하게 한다

---
+++ AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/game/EnemyGenerator.java
@@ -0,0 +1,30 @@
+package kr.ac.tukorea.ge.and.gwang.stealth.game;
+
+import android.graphics.Canvas;
+
+import java.util.Random;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+
+public class EnemyGenerator implements IGameObject {
+    private static final String TAG = EnemyGenerator.class.getSimpleName();
+    private final Random random = new Random();
+    private float enemyTime = 0;
+    @Override
+    public void update() {
+        enemyTime -= GameView.frameTime;
+        if ( enemyTime < 0 ){
+            int index = random.nextInt(5);
+            Scene.top().add(new Enemy(index));
+            enemyTime = random.nextFloat() + 0.5f;
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+}
--- AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/game/MainScene.java
+++ AndPrj/Stealth/app/src/main/java/kr/ac/tukorea/ge/and/gwang/stealth/game/MainScene.java
@@ -2,8 +2,6 @@ package kr.ac.tukorea.ge.and.gwang.stealth.game;
 
 import android.view.MotionEvent;
 
-import java.util.Random;
-
 import kr.ac.tukorea.ge.and.gwang.stealth.BuildConfig;
 import kr.ac.tukorea.ge.and.gwang.stealth.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.JoyStick;
@@ -17,8 +15,6 @@ public class MainScene extends Scene {
     private final Player player;
 
     private JoyStick joyStick;
-    private Random random = new Random();
-    private float eneyTime = 0;
 
     public MainScene() {
         Metrics.setGameSize(1600, 900);
@@ -26,34 +22,20 @@ public class MainScene extends Scene {
         GameView.drawsDebugStuffs = BuildConfig.DEBUG;
         // 디버그 빌드일 때만 나오게 한다
 
-
         joyStick = new JoyStick(
                 R.mipmap.bg_joystick, R.mipmap.thumb_joystick,
                 1380, 680,  120, 50,
                 100
         );
-
-
+        
         this.player = new Player(joyStick);
         add(player);
+        add(new EnemyGenerator());
         add(joyStick);
     }
 
     public boolean onTouchEvent(MotionEvent event) {
-
-
         return joyStick.onTouch(event);
     }
 
-    @Override
-    public void update() {
-        super.update();
-
-        eneyTime -= GameView.frameTime;
-        if ( eneyTime < 0 ) {
-            int index = random.nextInt(5);
-            add(new Enemy(index));
-            eneyTime = random.nextFloat() + 0.5f;
-        }
-    }
 }
